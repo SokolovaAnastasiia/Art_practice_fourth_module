0eb5c2359718900b62c6cbb0ab522c8b3541c3a7
SokolovaAnastasiia <71083444+SokolovaAnastasiia@users.noreply.github.com>
2021-06-19 01:20:23 +0300

    check

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-// $(document).scroll(function() {
-//     let y1 = parseInt($('body').css('padding'))
-//            + parseInt($('.header').css('height'));
-//     let y2 = $(this).scrollTop();
-//     if (y2 > y1) {
-//       $('.fixed-menu').fadeIn();
-//     } else {
-//       $('.fixed-menu').fadeOut();
-//     }
-//   });
+$(document).scroll(function() {
+    let y1 = parseInt($('body').css('padding'))
+           + parseInt($('.header').css('height'));
+    let y2 = $(this).scrollTop();
+    if (y2 > y1) {
+      $('.fixed-menu').fadeIn();
+    } else {
+      $('.fixed-menu').fadeOut();
+    }
+  });
 
