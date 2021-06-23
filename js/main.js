80569e330eda823c3ed21ea9276cc62f32961c51
SokolovaAnastasiia <aasokolova99@gmail.com>
2021-06-23 22:31:42 +0300

    lang5

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@ $(document).ready(function(){
                 missionChangeLanguage();
                 break;
             default:
-                indexChangeLanguage();
+                break;
             }
     }
     
